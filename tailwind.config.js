a13592c06fd069fa3a07f87349247fe14bc2f0fe
Limitless Programmer <aimlwork26@gmail.com>
2024-09-12 14:35:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+    "./public/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
